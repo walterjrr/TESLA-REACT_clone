9bc80b9e77d0dc79f5cd62c69adffac058148d39
walterjrr <jrwalter731@gmail.com>
2022-01-26 14:36:11 -0300

    front da pagina completa

--- src/App.js
+++ src/App.js
@@ -1,18 +1,39 @@
 import './App.css';
 import Header from './Component/Header'
 
+
+
+
 function App() {
   return (
     <>
-    <section>
-        <div className="background">
-          <Header />
-          <h1 className='model3'>Model 3</h1>
+      <nav className='scroll'>
+        <Header />
+        <div className="background model3">
+          <h1 className='models'>Model 3</h1>
           <h3 className='spanmodels'>Order Online for Touchless Delivery</h3>
           <button className='order'>CUSTOM ORDER</button>
           <button className='inventory'>EXISTING INVENTORY</button>
         </div>
-      </section>
+        <div className="background modelx">
+          <h1 className='models'>Model X</h1>
+          <h3 className='spanmodels'>Order Online for Touchless Delivery</h3>
+          <button className='order'>CUSTOM ORDER</button>
+          <button className='inventory'>EXISTING INVENTORY</button>
+        </div>
+        <div className="background modely">
+          <h1 className='models'>Model Y</h1>
+          <h3 className='spanmodels'>Order Online for Touchless Delivery</h3>
+          <button className='order'>CUSTOM ORDER</button>
+          <button className='inventory'>EXISTING INVENTORY</button>
+        </div>
+        <div className="background model-s">
+          <h1 className='models'>Model S</h1>
+          <h3 className='spanmodels'>Order Online for Touchless Delivery</h3>
+          <button className='order'>CUSTOM ORDER</button>
+          <button className='inventory'>EXISTING INVENTORY</button>
+        </div>
+      </nav>
     </>
   )
 }
